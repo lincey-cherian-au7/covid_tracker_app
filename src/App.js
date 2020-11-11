d332111c00378aebac355a17c99986af776a935a
lincey-cherian-au7 <linceycherian@gmail.com>
2020-11-11 11:59:15 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter,Switch,Link,Route} from 'react-router-dom';
+import National from './Components/National';
+import InterNational from './Components/InterNational'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+  }
+  render() {
+    return (
+      <div className = 'container-fluid'>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/'>
+              <National/>
+            </Route>
+            <Route exact path='/national'>
+              <National/>
+            </Route>
+            <Route exact path='/international'>
+              <InterNational/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
