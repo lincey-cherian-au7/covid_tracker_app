76ffdf70a7b74079e988f5d3887ee71bd4257087
lincey-cherian-au7 <linceycherian@gmail.com>
2020-11-11 17:22:52 +0530

    completed International

--- src/Components/InterNational.js
+++ src/Components/InterNational.js
@@ -1,9 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class InterNational extends Component {
+    constructor(){
+        super();
+        this.state ={
+            data:[]
+        }
+    }
+    componentDidMount(){
+        Axios.get('https://corona.lmao.ninja/v2/countries').then((res)=>{
+            this.setState({data:res.data})
+        })
+    }
     render() {
         return (
-            <div>
+            <div className="row">
+                <div className="col-md-12">
+                    <table className="table table-margin">
+                        <thead>
+                            <tr>
+                                <td>Country</td>
+                                <td>Total Cases</td>
+                                <td>Recovered</td>
+                                <td>Deaths</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.data && this.state.data.map((item,index)=>{
+                                return(
+                                    <tr>
+                                        <td>
+                                            {item.country}
+                                            <img style={{width:'50px', marginLeft:'10px'}} src={item.countryInfo.flag}></img></td>
+                                        <td>{item.cases}</td>
+                                        <td>{item.recovered}</td>
+                                        <td>{item.deaths}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
                 
             </div>
         )
